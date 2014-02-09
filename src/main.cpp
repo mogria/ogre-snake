a174eeacde8c5d587fe1e36e19611c0af852785b
Lucien Zuercher(PM) <l.zuercher@tincan.ch>
2014-02-09 18:17:55 +0200

    tabs with spaces replaced

--- src/main.cpp
+++ src/main.cpp
@@ -19,60 +19,60 @@
 int run()
 {
 	
-		Ogre::Root root("plugins.cfg");
-		if(!root.showConfigDialog()) return EXIT_FAILURE;
+  Ogre::Root root("plugins.cfg");
+  if(!root.showConfigDialog()) return EXIT_FAILURE;
 
-		std::unique_ptr<Ogre::RenderWindow>
-		window(root.initialise(true));
+  std::unique_ptr<Ogre::RenderWindow>
+  window(root.initialise(true));
 
-		Ogre::SceneManager* sceneManager =
-		root.createSceneManager(Ogre::ST_GENERIC);
+  Ogre::SceneManager* sceneManager =
+  root.createSceneManager(Ogre::ST_GENERIC);
 
-		// attach camera to scene
-		Ogre::Camera* camera = sceneManager->createCamera("MainCamera");
-		camera->setNearClipDistance(5);
-		camera->setNearClipDistance(1000);
-		Ogre::SceneNode* cameraNode =
-		sceneManager->getRootSceneNode()->createChildSceneNode();
-		cameraNode->attachObject(camera);
+  // attach camera to scene
+  Ogre::Camera* camera = sceneManager->createCamera("MainCamera");
+  camera->setNearClipDistance(5);
+  camera->setNearClipDistance(1000);
+  Ogre::SceneNode* cameraNode =
+  sceneManager->getRootSceneNode()->createChildSceneNode();
+  cameraNode->attachObject(camera);
 
-		// render camera to window;
-		Ogre::Viewport* viewport = window->addViewport(camera);
-		viewport->setClearEveryFrame(true);
+  // render camera to window;
+  Ogre::Viewport* viewport = window->addViewport(camera);
+  viewport->setClearEveryFrame(true);
 
-		// capture input
-		size_t windowHnd;
-		window->getCustomAttribute("WINDOW", &windowHnd);
-		std::ostringstream windowHndStream;
-		windowHndStream << windowHnd;
-		std::string windowHndStr = windowHndStream.str();
+  // capture input
+  size_t windowHnd;
+  window->getCustomAttribute("WINDOW", &windowHnd);
+  std::ostringstream windowHndStream;
+  windowHndStream << windowHnd;
+  std::string windowHndStr = windowHndStream.str();
 
-		OIS::ParamList specialParameters;
-	//#ifdef OIS_LINUX_PLATFORM
-		specialParameters.insert(std::make_pair(std::string("x11_mouse_grab"), std::string("false")));
-		specialParameters.insert(std::make_pair(std::string("x11_mouse_hide"), std::string("false")));
-		specialParameters.insert(std::make_pair(std::string("x11_keyboard_grab"), std::string("false")));
-		specialParameters.insert(std::make_pair(std::string("XAutoRepeatOn"), std::string("true")));
-	//#endif
-		specialParameters.insert(std::make_pair(std::string("WINDOW"), windowHndStr));
+  OIS::ParamList specialParameters;
+  //#ifdef OIS_LINUX_PLATFORM
+    specialParameters.insert(std::make_pair(std::string("x11_mouse_grab"), std::string("false")));
+    specialParameters.insert(std::make_pair(std::string("x11_mouse_hide"), std::string("false")));
+    specialParameters.insert(std::make_pair(std::string("x11_keyboard_grab"), std::string("false")));
+    specialParameters.insert(std::make_pair(std::string("XAutoRepeatOn"), std::string("true")));
+  //#endif
+  specialParameters.insert(std::make_pair(std::string("WINDOW"), windowHndStr));
 
-		OIS::InputManager* inputManager = OIS::InputManager::createInputSystem(specialParameters);
+  OIS::InputManager* inputManager = OIS::InputManager::createInputSystem(specialParameters);
 
-		OIS::Keyboard* keyboard = static_cast<OIS::Keyboard*>(inputManager->createInputObject(OIS::OISKeyboard, false));
-		OIS::Mouse* mouse = static_cast<OIS::Mouse*>(inputManager->createInputObject(OIS::OISMouse, false));
+  OIS::Keyboard* keyboard = static_cast<OIS::Keyboard*>(inputManager->createInputObject(OIS::OISKeyboard, false));
+  OIS::Mouse* mouse = static_cast<OIS::Mouse*>(inputManager->createInputObject(OIS::OISMouse, false));
 
-		View::Main view(*window.get(), *sceneManager, *camera, *cameraNode, *viewport);
+  View::Main view(*window.get(), *sceneManager, *camera, *cameraNode, *viewport);
 
-		Controller::Main controller(view);
+  Controller::Main controller(view);
 
-		controller.on_start();
-		while(!window->isClosed() && !controller.is_game_finished()) {
-		controller.on_key_input(*keyboard);
-		controller.on_mouse_input(*mouse);
-		controller.on_frame();
-		root.renderOneFrame();
-		}
-		controller.on_end();
+  controller.on_start();
+  while(!window->isClosed() && !controller.is_game_finished()) {
+	  controller.on_key_input(*keyboard);
+	  controller.on_mouse_input(*mouse);
+	  controller.on_frame();
+	  root.renderOneFrame();
+  }
+  controller.on_end();
 
 
   delete mouse;
@@ -81,7 +81,7 @@ int run()
   delete camera;
   delete sceneManager;
 
-    return 0;
+  return 0;
 }
  
 #ifdef __cplusplus
@@ -89,25 +89,25 @@ extern "C" {
 #endif
  
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-    INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+  INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
 #else
-    int main(int argc, char *argv[])
+  int main(int argc, char *argv[])
 #endif
-    {
+{
  
-        try 
-        {
-            run();
-        } catch( Ogre::Exception& e ) {
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-            //MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR| MB_TASKMODAL);
-#else
-            std::cerr << "An exception has occured: " << e.getFullDescription().c_str() << std::endl;
-#endif
-        }
+  try 
+  {
+    //run();
+  } catch( Ogre::Exception& e ) {
+  #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    //MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR| MB_TASKMODAL); 
+  #else
+    std::cerr << "An exception has occured: " << e.getFullDescription().c_str() << std::endl;
+  #endif
+  }
  
-        return 0;
-    }
+return 0;
+}
  
 #ifdef __cplusplus
 }
