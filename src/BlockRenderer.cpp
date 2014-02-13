6b1d9cd97c66a59648712820166708ca315fceff
Mogria <m0gr14@gmail.com>
2014-02-13 22:13:42 +0100

    enourmous performance improvement by caching enities

--- src/BlockRenderer.cpp
+++ src/BlockRenderer.cpp
@@ -3,32 +3,40 @@
 #include <OgreResourceGroupManager.h>
 
 BlockRenderer::BlockRenderer(Ogre::SceneManager* _scene)
-  : mSceneMgr(_scene)
-{
+  : mSceneMgr(_scene) {
   Ogre::MeshManager& meshManager = Ogre::MeshManager::getSingleton();
   mMesh = meshManager.load("cube.mesh",
       Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
 }
 
 BlockRenderer::~BlockRenderer() {
-  /* for(std::list<Ogre::SceneNode*>::iterator it = mCubes.begin; mCubes.back != it; it++)
+  for(auto it = mCubes.begin(); mCubes.end() != it; it++)
   {
-    it->
-  } */
+    it->second->removeAndDestroyAllChildren();
+    mSceneMgr->destroySceneNode(it->second);
+  }
 }
 
+Ogre::SceneNode* BlockRenderer::getOrCreateBlock(Model::map_coords xy) {
+  if(mCubes.find(xy) == mCubes.end()) {
+    float scaling = 0.1;
+    Ogre::Entity* entity = mSceneMgr->createEntity(mMesh);
+    Ogre::SceneNode* node = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    
+    node->setScale(scaling, scaling, scaling);
+    auto box_size = entity->getBoundingBox().getSize();
+    node->setPosition(box_size.x * xy.first * scaling,
+                      box_size.y * xy.second * scaling,
+                      0.0);
+    node->attachObject(entity);
+
+    mCubes[xy] = node;
+  }
+  return mCubes[xy];
+}
 
 void BlockRenderer::render(Model::map_coords xy, const Model::Block& block) {
-  float scaling = 0.1;
-  Ogre::Entity* entity = mSceneMgr->createEntity(mMesh);
-  Ogre::SceneNode* node = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-  
-  node->setScale(scaling, scaling, scaling);
-  auto box_size = entity->getBoundingBox().getSize();
-  node->setPosition(box_size.x * xy.first * scaling,
-                    box_size.y * xy.second * scaling,
-                    0.0);
-  node->attachObject(entity);
+  getOrCreateBlock(xy);
 }
 
 void BlockRenderer::render(Model::map_coords xy, const Model::EmptyBlock& block) {
--- src/BlockRenderer.h
+++ src/BlockRenderer.h
@@ -6,20 +6,21 @@
 #include <OgreSceneManager.h>
 #include <OgreEntity.h>
 #include <OgreMesh.h>
-#include <OgreMeshManager.h>
 
 class BlockRenderer {
   private:
     Ogre::SceneManager* mSceneMgr;
     Ogre::MeshPtr mMesh;
 
-    std::list<Ogre::SceneNode*> mCubes; // contains a list of all cubes to prevent reloading
+    std::map<Model::map_coords, Ogre::SceneNode*> mCubes; // contains a list of all cubes to prevent reloading
                                         // simple workaround
     // TODO list object contains all active cubes as pointers (Nodes) and removes them if they are not needed anymore
     // -> first object is original cube from witch all the others are cloned .. hope that helps
   public:
     BlockRenderer(Ogre::SceneManager* scene);
     virtual ~BlockRenderer();
+
+    Ogre::SceneNode* getOrCreateBlock(Model::map_coords xy);
 
     virtual void render(Model::map_coords xy, const Model::Block& block);
     virtual void render(Model::map_coords xy, const Model::EmptyBlock& block);
